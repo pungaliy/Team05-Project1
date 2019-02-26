05fa090b36d584153b209e7d76a3b2e8a9c1afed
Shray Pungaliya <pungaliy@usc.edu>
2019-02-26 14:53:23 -0800

    Setting up java files

---
+++ src/Recipe.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Recipe extends Item {
+
+	public String imageLink;
+	public double prepTime;
+	public double cookTime;
+	public ArrayList<String> ingredients;
+	public ArrayList<String> instructions;
+}
---
+++ src/SearchResult_java.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class SearchResult {
+	public int numItems;
+	public ArrayList <Item> recipeResults;
+	public ArrayList <Item> restaurantResults;
+	public String searchTerm;
+	public ArrayList <String> collage;
+}
