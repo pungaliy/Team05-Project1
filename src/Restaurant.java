3af8b82f2bfd875df7108dc49d1d6903fe7673ea
Shray Pungaliya <pungaliy@usc.edu>
2019-02-27 10:21:51 -0800

    No idea what's happening

--- src/Restaurant.java
+++
@@ -1,8 +0,0 @@
-
-public class Restaurant extends Item{
-	public String address;
-	public String phoneNumber;
-	public String googleMapsLink;
-	
-	
-}
--- src/SearchResult_java.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class SearchResult {
-	public int numItems;
-	public ArrayList <Item> recipeResults;
-	public ArrayList <Item> restaurantResults;
-	public String searchTerm;
-	public ArrayList <String> collage;
-}
