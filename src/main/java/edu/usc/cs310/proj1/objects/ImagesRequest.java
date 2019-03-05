10ce8d8116b9ff8eee140ca4784f81f6eb8c6ca6
ctbuckley <cbuckley@usc.edu>
2019-03-04 22:40:08 -0800

    pushing imagereqest updates

--- src/main/java/edu/usc/cs310/proj1/objects/ImagesRequest.java
+++ src/main/java/edu/usc/cs310/proj1/objects/ImagesRequest.java
@@ -1,11 +1,13 @@
 package edu.usc.cs310.proj1.objects;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class ImagesRequest {
 	
@@ -19,48 +21,51 @@ public class ImagesRequest {
 	
 	public ImagesRequest(String query) {
 		searchTerm = query;
+		this.request();
 	}
 	
 	private void request() {
 		
-		//first step is to grab numResults recipes to get from the search page
-		//based on https://www.allrecipes.com/search/results/?wt=chicken&sort=re
+		urlOfSearch = "https://www.googleapis.com/customsearch/v1?q=" + searchTerm + "&cx=010424164654780185781%3Adwdzcylugra&num=10&searchType=image&key=AIzaSyCUBryJarHnnYvjEdJuI48R2ATEv3OvuP4";
 		
-		urlOfSearch = "https://www.google.com/search?q=" + searchTerm + "&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi3zPbF5ebgAhUn34MKHQcRDwEQ_AUIDygC&biw=1440&bih=711";
-
-		WebClient client = new WebClient();  
-		client.getOptions().setCssEnabled(false);  
-		client.getOptions().setJavaScriptEnabled(false);
+		String storeJson = "";
 		
-		HtmlPage page = null;
-		
-		try {  
-		  page = client.getPage(urlOfSearch);
-		}catch(Exception e){
-		  e.printStackTrace();
+		try {
+			
+			//GET request to custom search api
+			
+			URL url = new URL(urlOfSearch);
+			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+			
+			String strTemp = "";
+			
+			//Store results of api call to storeJson string
+			
+			while ((strTemp = br.readLine()) != null) {
+				storeJson += strTemp;
+				storeJson += "\n";
+			}
+			
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
 		
-		//from this page, we need to pull the following values
+		//pull links from stored json response
 		
-		List<HtmlElement> items = (List<HtmlElement>) page.getByXPath("//img"); 
-		
-		for (int i = 0; i < 10; i++) {
-			
-			
-			String imageLink;
+		JsonObject jsonObject = new JsonParser().parse(storeJson).getAsJsonObject();
 
-			items = (List<HtmlElement>) page.getByXPath("//img");
-			imageLink = items.get(0).getAttribute("src");
-			
-			imageResultURLs.add(imageLink);
-		}
-		
+        JsonArray arr = jsonObject.getAsJsonArray("items");
+        
+        for (int i = 0; i < arr.size(); i++) {
+            String link = arr.get(i).getAsJsonObject().get("link").getAsString();
+            imageResultURLs.add(link);
+        }
+        
 	}
 
 	public static void main (String args []) {
-		ImagesRequest ir = new ImagesRequest("carrot");
-		
-		
+		ImagesRequest ir = new ImagesRequest("chicken");
 	}
 
 }
